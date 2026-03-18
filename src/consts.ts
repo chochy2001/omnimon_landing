4a21cff07002069615076912c351ac07477f59ca
Jorge Salgado Miranda <jorge@MacBook-Pro-de-Jorge.local>
2026-03-18 07:32:46 -0600

    chore: bump version to v6.6.5

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,5 @@
  * Update version here and all pages will pick it up.
  */
 
-export const OMNIMON_VERSION = '6.6.4';
+export const OMNIMON_VERSION = '6.6.5';
 export const RELEASE_DATE = '17 Mar 2026';
