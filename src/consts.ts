0dd8abf46ad8ee93d26c08e5a00c7c2cba277c2b
chochy2001 <54371626+chochy2001@users.noreply.github.com>
2026-09-19 15:28:50 -0600

    docs(blog): OmniMon v6.8.0 Memory Guard (EN + ES)
    Landing copy for the open-source macOS LaunchAgent. Manifests may show
    6.8.0 while the latest published tag remains v6.6.6.

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@
  * Update version here and all pages will pick it up.
  */
 
-export const OMNIMON_VERSION = '6.7.0';
-export const RELEASE_DATE = '17 Apr 2026';
-export const RELEASE_DATE_ES = '17 Abr 2026';
+export const OMNIMON_VERSION = '6.8.0';
+export const RELEASE_DATE = '19 Sep 2026';
+export const RELEASE_DATE_ES = '19 Sep 2026';
